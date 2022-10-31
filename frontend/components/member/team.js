9086285dcbc2c32244d678fb94ebe92e0b0a86b0
Austin <austin.jiuk.kim@gmail.com>
2022-11-01 05:20:25 +0900

    remove .

--- frontend/components/member/team.js
+++ frontend/components/member/team.js
@@ -13,7 +13,7 @@ export default function Team() {
               <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
                 Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
                 gentrify, subway tile poke farm-to-table. Franzen you probably
-                haven't heard of them.
+                havent heard of them.
               </p>
             </div>
             <div className="flex flex-wrap -m-4">
