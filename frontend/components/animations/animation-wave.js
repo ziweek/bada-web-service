8e1d1d96999e0d31851d3ca0b20e88099cafca68
Austin <austin.jiuk.kim@gmail.com>
2022-11-01 06:51:31 +0900

    Add Darkmode

--- frontend/components/animations/animation-wave.js
+++ frontend/components/animations/animation-wave.js
@@ -10,7 +10,7 @@ export default function AnimationWave() {
       loop
       animationData={lottieJson}
       play
-      // style={{ width: 720, height: 500 }}
+      style={{ width: `100%`, height: `100%` }}
     />
   );
 }
