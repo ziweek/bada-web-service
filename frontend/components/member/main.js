5d72d7fede5a5085343acbd9310ec1ecd7e4e0fc
Alex Jiuk Kim <alex.jiuk.kim@gmail.com>
2023-01-28 01:14:04 +0900

    .

--- frontend/components/member/main.js
+++ frontend/components/member/main.js
@@ -32,7 +32,7 @@ export default function MemberMain() {
           <div className="flex flex-wrap -m-4">
             {/*  */}
             {res.map((e, i) => (
-              <div className="p-4 lg:w-1/4 md:w-1/2 pb-8">
+              <div key={i} className="p-4 lg:w-1/4 md:w-1/2 pb-8">
                 <div className="h-full flex flex-col items-center text-center">
                   <Image
                     priority={true}
--- frontend/components/project/content.js
+++ frontend/components/project/content.js
@@ -31,7 +31,7 @@ export default function Content() {
           </div>
           <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
             {res.map((e, i) => (
-              <div className="p-4 md:w-1/3 sm:mb-0 mb-6 pb-8">
+              <div key={i} className="p-4 md:w-1/3 sm:mb-0 mb-6 pb-8">
                 <div className="rounded-lg h-64 overflow-hidden">
                   <Image
                     width={1200}
